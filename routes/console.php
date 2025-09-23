2965756a5430065d4f268a3512ae5bf952314060
ccaioadriano <caiobhadriano@gmail.com>
2025-09-22 23:19:43 -0300

    Adicionando geração de sitemap e configurando dependências no composer

--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,13 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Spatie\Sitemap\SitemapGenerator;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+Artisan::command('sitemap:generate', function () {
+    SitemapGenerator::create('https://www.caioadriano.com.br')
+        ->writeToFile(public_path('sitemap.xml'));
+})->purpose('Gerar o sitemap do site');
